dcba3e692a771efcc2e5ad7b0cedac382a497968
Dhurim <dhkelmendi@gmail.com>
2017-05-26 00:35:00 +0300

    Add destroy todo_item action

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,8 +1,18 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  def destroy
+    if @todo_item.destroy
+      flash[:success] = "Todo list item was deleted"
+    else
+      flas[:error] = "Todo list item could not be deleted"
+    end
     redirect_to @todo_list
   end
 
@@ -10,6 +20,10 @@ class TodoItemsController < ApplicationController
 
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def todo_item_params
